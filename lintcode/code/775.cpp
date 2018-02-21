fc1c5bac4456cfeaa7ff934117d54c9ac5b88e34
kylinZhuo <kylin.zhuo@gmail.com>
2018-02-20 21:57:42 -0500

    02/20/2018 21:57

---
+++ lintcode/code/775.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    /**
+     * @param words: a list of unique words
+     * @return: all pairs of distinct indices
+     */
+    vector<vector<int>> palindromePairs(vector<string> &words) {
+        // Write your code here
+        vector<vector<int>> res;
+        unordered_map<string, int> m;
+        set<int> s;
+        for (int i = 0; i < words.size(); ++i) {
+            m[words[i]] = i;
+            s.insert(words[i].size());
+        }
+        for (int i = 0; i < words.size(); ++i) {
+            string t = words[i];
+            int len = t.size();
+            reverse(t.begin(), t.end());
+            if (m.count(t) && m[t] != i) {
+                res.push_back({i, m[t]});
+            }
+            auto a = s.find(len);
+            for (auto it = s.begin(); it != a; ++it) {
+                int d = *it;
+                if (isValid(t, 0, len - d - 1) && m.count(t.substr(len - d))) {
+                    res.push_back({i, m[t.substr(len - d)]});
+                }
+                if (isValid(t, d, len - 1) && m.count(t.substr(0, d))) {
+                    res.push_back({m[t.substr(0, d)], i});
+                }
+            }
+        }
+        return res;
+    }
+    
+    bool isValid(string t, int left, int right) {
+        while (left < right) {
+            if (t[left++] != t[right--]) return false;
+        }
+        return true;
+    }
+};
